f081facf8a606241134551674ab7a2dc4e7821be
dustin1709 <56805815+dustin1709@users.noreply.github.com>
2020-05-05 18:04:54 -0400

    Add files via upload

---
+++ app/tab2/tab2.module.ts
@@ -0,0 +1,21 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { HTTP } from '@ionic-native/http/ngx';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    RouterModule.forChild([{ path: '', component: Tab2Page }])
+  ],
+  providers: [HTTP],
+  declarations: [Tab2Page]
+})
+export class Tab2PageModule {}
