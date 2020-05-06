1d63ab55de8bcd5b5f4ed01f425da787d737447c
dustin1709 <56805815+dustin1709@users.noreply.github.com>
2020-05-05 21:19:18 -0400

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-
-//IMPORT THE PLUGINS
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 
@@ -22,7 +17,6 @@ import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
   providers: [
     StatusBar,
     SplashScreen,
-    //ADD GEOLOCATION Y GEOCODER ON THE PROVIDERS.
     Geolocation,    
     NativeGeocoder,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
